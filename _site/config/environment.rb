798fd2420a203ebb435cdb4a594455f51c8f4f5b
Steven Olson <steveoscaro@gmail.com>
2016-02-21 13:52:47 -0700

    automated movie poster scraping if poster does not already exist

---
+++ _site/config/environment.rb
@@ -0,0 +1,19 @@
+require 'bundler'
+Bundler.require
+require 'tilt/erb'
+# get the path of the root of the app
+APP_ROOT = File.expand_path("..", __dir__)
+
+# require the controller(s)
+Dir.glob(File.join(APP_ROOT, 'app', 'controllers', '*.rb')).each { |file| require file }
+
+# require the model(s)
+Dir.glob(File.join(APP_ROOT, 'app', 'models', '*.rb')).each { |file| require file }
+
+# configure TaskManagerApp settings
+class MoviesApp < Sinatra::Base
+  set :method_override, true
+  set :root, APP_ROOT
+  set :views, File.join(APP_ROOT, "app", "views")
+  set :public_folder, File.join(APP_ROOT, "app", "public")
+end
